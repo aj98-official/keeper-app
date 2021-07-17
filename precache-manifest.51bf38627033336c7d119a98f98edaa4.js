587619d33d553e33264c81bdb9d840df336ea6ec
Ashutosh Jaiswal <ashutoshjais98@gmail.com>
2021-07-17 19:21:20 +0530

    Updates

---
+++ precache-manifest.51bf38627033336c7d119a98f98edaa4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ee80dd1e5e461e1b55e44c80aed17e",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "b5031a55b0de8577b66d",
+    "url": "/keeper-app/static/js/2.d3d645ac.chunk.js"
+  },
+  {
+    "revision": "58b4ae287e712fd2007d",
+    "url": "/keeper-app/static/js/main.4ecc74e2.chunk.js"
+  },
+  {
+    "revision": "4a6f66a56d93c5ad3e45",
+    "url": "/keeper-app/static/js/runtime-main.c4e5d55e.js"
+  }
+]);
